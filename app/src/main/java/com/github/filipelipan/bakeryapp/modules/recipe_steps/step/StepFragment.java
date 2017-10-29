b3d6bfd66d1a1ef72797538a7261bf0793422abd
Filipe Schmidt <lispa9@hotmail.com>
2017-10-28 21:03:53 -0300

    add isTablet boolean

--- app/src/main/java/com/github/filipelipan/bakeryapp/modules/recipe_steps/step/StepFragment.java
+++ app/src/main/java/com/github/filipelipan/bakeryapp/modules/recipe_steps/step/StepFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.github.filipelipan.bakeryapp.R;
 import com.github.filipelipan.bakeryapp.common.AppFragment;
 import com.github.filipelipan.bakeryapp.data.model.Step;
 import com.github.filipelipan.bakeryapp.modules.recipe_steps.CardAdapter;
 import com.github.filipelipan.bakeryapp.modules.recipe_steps.IRecipeStepsView;
 import com.github.filipelipan.bakeryapp.modules.recipe_steps.RecipeStepsPresenter;
+
+import butterknife.BindView;
 
 /**
  * Created by lispa on 22/10/2017.
@@ -19,10 +25,15 @@ import com.github.filipelipan.bakeryapp.modules.recipe_steps.RecipeStepsPresente
 public class StepFragment extends AppFragment<IRecipeStepsView, RecipeStepsPresenter> implements IStepView {
 
 	public static final String STEP_KEY = "STEP_KEY";
-	private CardView mCardView;
 
 	private Step mStep;
 
+	@BindView(R.id.description_tv)
+	TextView descriptionTextView;
+	@BindView(R.id.video_iv)
+	ImageView videoImageView;
+	@BindView(R.id.cardView)
+	CardView mCardView;
 
 	public static StepFragment newInstance(Step step){
 		StepFragment stepFragment = new StepFragment();
@@ -38,12 +49,16 @@ public class StepFragment extends AppFragment<IRecipeStepsView, RecipeStepsPrese
 
 		if(getArguments().containsKey(STEP_KEY)){
 			mStep = getArguments().getParcelable(STEP_KEY);
+
+			Glide.with(getContext())
+					.load(mStep.getThumbnailURL())
+					.apply(RequestOptions.centerCropTransform())
+					.into(videoImageView);
+			descriptionTextView.setText(mStep.getDescription());
 		}
 
-		mCardView = (CardView) view.findViewById(R.id.cardView);
 		mCardView.setMaxCardElevation(mCardView.getCardElevation()
 				* CardAdapter.MAX_ELEVATION_FACTOR);
-
 
 
 	}
